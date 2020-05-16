66df7cfa10ec7c38e3d9cfd982dc71d36fdbd4df
Ayaz Ahmed <ayez_ahmed@hotmail.com>
2020-05-16 07:03:26 +0500

    added option for hot auto reload in dev server

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -11,7 +11,8 @@ module.exports = merge(common, {
   },
   devServer: {
     inline: true,
-    hot: true
+    hot: true,
+    watchContentBase: true
   },
   plugins: [
     new Webpack.DefinePlugin({
