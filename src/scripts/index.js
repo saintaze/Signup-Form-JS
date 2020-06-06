3defc2e46e2bc4689daa88c7604b2c62e547f128
saintaze <ayez_ahmed@hotmail.com>
2020-06-06 06:13:09 +0500

    functions refactored

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,43 +18,51 @@ const validControls = {
   password: false
 }
 
-// Moving Label On Focus
+// Move Label On Focus
 $inputs.forEach(input => {
   input.addEventListener('focus', e => {
     input.nextElementSibling.classList.add('form__label--moveup');
   });
 });
 
-// Error Class Toggle 
+
+// Add Error On Touched
 $controls.forEach(control => {
-  if(!control.id) return
-  const controlType = control.id.split('--')[1];
-  q('input', control).addEventListener('keyup', e => {
-    const val = e.target.value;
-    const isValid = validationRules[controlType].every(rule => validator[rule](val))
-    if(isValid){
-      q('.form__label', control).classList.remove('form__label--error');
-      q('.form__input', control).classList.remove('form__input--error');
-      q('.form__error-message', control).classList.remove('form__error-message--show');
-      validControls[controlType] = true
-    }else {
-      q('.form__label', control).classList.add('form__label--error');
-      q('.form__input', control).classList.add('form__input--error');
-      q('.form__error-message', control).classList.add('form__error-message--show');
-      validControls[controlType] = false
-    }
-    toggleBtnDisable()
+  if (!control.id) return;
+  q('input', control).addEventListener('blur', ({ target: { value } }) => {
+    let toggle = !validator.isNotEmpty(value) ? 'add' : 'remove';
+    toggleErrorClass(control, toggle);
   });
 });
 
 
+// Toggle Error On Type 
+$controls.forEach(control => {
+  if(!control.id) return;
+  const controlType = control.id.split('--')[1];
+  q('input', control).addEventListener('keyup', ({ target: { value } }) => {
+    const isValid = validationRules[controlType].every(rule => validator[rule](value));
+    let toggle = isValid ? 'remove' : 'add';
+    validControls[controlType] = toggle === 'remove' ? true: false; 
+    toggleErrorClass(control, toggle); 
+    toggleBtnDisable();
+  });
+});
+
+
+// Toggle Error Class
+const toggleErrorClass = (control, toggle) => {
+  q('.form__label', control).classList[toggle]('form__label--error');
+  q('.form__input', control).classList[toggle]('form__input--error');
+  q('.form__error-message', control).classList[toggle]('form__error-message--show');
+}
+
+
+// Toggle Next Button Disabled
 const toggleBtnDisable = () => {
   const formValid = Object.keys(validControls).every(control => validControls[control]);
-  if(formValid){
-    q('.form__next').classList.remove('form__next--disabled');
-  }else{
-    q('.form__next').classList.add('form__next--disabled');
-  }
+  let toggle = formValid ? 'remove'  : 'add';
+  q('.form__next').classList[toggle]('form__next--disabled');
 }
 
 
